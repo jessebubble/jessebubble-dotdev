ba943b0ea5b5bf68cb8c0b990a6f5550c2b0453b
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-08-09 14:41:16 -0500

    added content to pages about contact portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,10 +3,10 @@ import { ImInstagram, ImFacebook2, ImTwitter, ImGithub, ImLinkedin } from "react
 import { IconContext } from 'react-icons';
 import { Player } from '@lottiefiles/react-lottie-player';
 
-export default function App() {
+export default function Portfolio() {
   return (
     <>
-    <nav className='px-2 sm:px-4 py-2.5 sticky top-0 bg-black'>
+     <nav className='px-2 sm:px-4 py-2.5 sticky top-0 bg-black'>
       <div className='container flex flex-wrap justify-between items-center mx-auto'>
         <Link to='/' className='flex items-center'>
           <img src='./logo192.png' alt='collage icon' className='pr-1 h-8 sm:h-9' />
@@ -40,46 +40,16 @@ export default function App() {
           <span className='text-rose-500'> Development</span> 
         </h2>
         <p className='text-lg pt-1 xl:pt-2 text-center tracking-tight subpixel-antialiased text-gray-900 pb-1 xl:pb-2'>
-          More than just a developer
+          More than just a portfolio
         </p>
       </div>
     </main>
-    <main className='bg-white'>
-      <div className='flex flex-col xl:flex-row items-center justify-center pt-5 pb-5 md:pt-6 md:pb-6 xl:pb-12 xl:pt-12 xl:px-1'>
-        <div className='container inline-grid grid-cols-2 pl-1 md:pl-2 xl:pl-3'> 
-          <img src='../bubble.png' alt='portrait' className=' bg-amber-400 rotate-6' />
-          <img src='../bubble.png' alt='portrait' className=' bg-emerald-400 hover:-rotate-6 ' />
-          <img src='../bubble.png' alt='portrait' className=' bg-rose-400 hover:rotate-6 ' />
-          <img src='../bubble.png' alt='portrait' className=' bg-fuchsia-400 -rotate-6 ' />
-        </div>
-        <div className='flex flex-col items-center justify-center p-8 md:p-10 xl:p-12'>
-          <div className='container inline-flex flex-col'>
-            <p className='text-center xl:text-start tracking-tighter subpixel-antialiased text-md md:text-xl xl:text-2xl font-light'>
-              Freelance Web Developer with <span className='text-rose-500'>strong interpersonal and communication skills</span> who is excited to leverage his skills as part of a fast-paced, quality-driven team.
-              <br /> 
-              <br />
-              From the world of selling insurance to holding a Full Stack Web Development certificate from the University of Texas at San Antonio where I developed skills using <span className='text-rose-500'>modern web technologies.</span> 
-              <br />
-              <br />
-              Through my freelance work with Viva Web Design i've gained experience with registering domain names and setting up DNS servers, <span className='text-rose-500'>testing web pages for performance, responsiveness and search engine optimization.</span>
-          </p>
-          </div>
-        </div>
-      </div>
-    </main>
-    <main className="flex flex-col">
-      <div className="px-4 py-6 md:py-7 xl:py-8 bg-black">
-        <h1 className="text-3xl md:text-4xl xl:text-5xl text-center tracking-tight text-white">
-          More Than Just A <span className='text-rose-500'>Portfolio</span>
-        </h1>
-      </div>
-    </main>
-    <main className='bg-gradient-to-b from-black via-teal-300 to-white'>
+    <main className='bg-gradient-to-t from-rose-500 via-rose-300 to-white'>
       <div className='flex flex-col xl:flex-row w-full items-center justify-center pb-4'>
           <div className='w-9/12 xl:w-5/12 inline-grid grid-col items-center '> 
           <div className='overflow-hidden pt-4 xl:p-12 xl:px-20'>
               <Player
-              src='https://assets2.lottiefiles.com/packages/lf20_2t5zvcho.json'
+              src='https://assets7.lottiefiles.com/packages/lf20_4tcribvp.json'
               className='xl:pb-2'
               background='transparent'
               loop={true}
@@ -127,11 +97,11 @@ export default function App() {
           </div>
       </div>
     </main>
-    <main className='bg-gradient-to-b from-white via-fuchsia-400 to-fuchsia-500'>
+    <main className='bg-gradient-to-t from-lime-200 via-lime-400 to-rose-500'>
       <div className='flex flex-col xl:flex-row items-center justify-center pt-2 pb-2 md:pb-4 px-10 md:px-60 xl:px-80'>
         <div className='inline-grid grid-col items-center '> 
           <div className='overflow-hidden pt-2'>
-            <img src='../caroline-unsplash.png' alt='portrait' className='xl:w-9/12 xl:pl-4 xl:rounded-lg hover:rotate-6' />
+            <img src='../bianca-berg-unsplash.png' alt='portrait' className='xl:w-8/12 xl:pl-4 xl:rounded-lg hover:rotate-6' />
           </div>
         </div>
         <div className='inline-grid grid-col w-full'> 
@@ -166,56 +136,6 @@ export default function App() {
             </div>
           </div>
         </div>
-      </div>
-    </main>
-    <main className="flex flex-col">
-      <div className="px-4 py-6 md:py-7 xl:py-8 bg-black">
-        <h1 className="text-3xl md:text-4xl xl:text-5xl text-center tracking-tight text-white">
-          More Than Just A <span className='text-rose-500'>Contact</span>
-        </h1>
-      </div>
-    </main>
-    <main className='bg-gradient-to-b from-black via-cyan-300 to-white'>
-      <div className='flex flex-col xl:flex-row w-full items-center justify-center pb-4'>
-          <div className='w-9/12 xl:w-5/12 inline-grid grid-col items-center '> 
-          <div className='overflow-hidden pt-4 xl:p-12 xl:px-20'>
-              <Player
-              src='https://assets1.lottiefiles.com/private_files/lf30_ies8kilu.json'
-              className='xl:pb-2'
-              background='transparent'
-              loop={true}
-              autoplay={true} >
-              </Player>
-          </div>
-          </div>
-          <div className='inline-grid grid-col'> 
-              <div className='pt-4 xl:pr-36'>
-                  <h2 className='text-center text-3xl xl:text-5xl pb-1 xl:pb-2 font-light subpixel-antialiased tracking-tight text-gray-900'>
-                    The Future is <span className='text-white'>Transparency</span>
-                  </h2>
-                  <p className='pb-1 xl:pb-3 text-center text-lg xl:text-xl tracking-tight subpixel-antialiased text-gray-900 font-light'>
-                    Independent doesn't mean alone
-                  </p>
-              <div className='pb-3 xl:pb-6 p-2 text-center shadow-sm shadow-current text-lg md:text-2xl font-medium lg:p-5
-                          transform hover:translate-y-4 transition duration-300 ease-in-out'>
-              <div className='text-gray-900 hover:text-rose-500 '>
-                Give me a call
-              </div>
-              </div>
-              <div className='pb-3 xl:pb-6 p-2 text-center shadow-sm shadow-current bg-transparent text-lg md:text-2xl font-medium lg:p-5
-                          transform hover:translate-y-4 transition duration-300 ease-in-out'>
-              <div className='text-gray-900 hover:text-rose-500 '>
-                Send me an email
-              </div>
-              </div>
-              <div className='pb-3 xl:pb-6 p-2 text-center shadow-sm shadow-current bg-transparent text-lg md:text-2xl font-medium lg:p-5
-                          transform hover:translate-y-4 transition duration-300 ease-in-out'>
-              <div className='text-gray-900 hover:text-rose-500 '>
-                Schedule a remote meetup
-              </div>
-              </div>
-              </div>
-          </div>
       </div>
     </main>
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>
 );
