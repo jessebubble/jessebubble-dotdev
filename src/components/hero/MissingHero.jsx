715366db45d76bbae5d4865ce75fa652c37c8713
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2023-04-03 17:57:58 -0500

    replaced page routes with hero tabs, hero svg added to bg

--- src/components/hero/MissingHero.jsx
+++ src/components/hero/MissingHero.jsx
@@ -1,17 +1,13 @@
-import SideBar from "../sidebar/SideBar"
+import Pattern from '../Pattern';
 import { Player } from '@lottiefiles/react-lottie-player';
+import { Link } from 'react-router-dom';
   
   export default function MissingHero() {
       
     return (
-        <div className="bg-slate-900 py-24 sm:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-36">
-                <div className="mx-auto grid max-w-2xl grid-cols-1 items-start gap-y-16 gap-x-8 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                    <div className="lg:pr-4">
-                        <div className="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
-                            <SideBar />
-                        </div>
-                    </div>
+        <div className="bg-slate-900">
+            <div className="mx-auto max-w-7xl">
+                <div className="relative isolate overflow-hidden bg-slate-900 px-6 py-24 shadow-2xl sm:rounded-3xl sm:px-24 xl:py-32">
                     <div className="bg-white rounded-2xl">
                         <Player
                             src="https://assets5.lottiefiles.com/packages/lf20_kjixtysj.json"
@@ -21,6 +17,14 @@ import { Player } from '@lottiefiles/react-lottie-player';
                             autoplay={true} >
                         </Player>
                     </div>
+                    <Link to="/">
+                        <div className="relative z-10 max-w-3xl mx-auto text-center p-8 mt-8">
+                            <button className="bg-logo-pink text-white hover:bg-indigo-500 font-bold py-2 px-4 rounded-2xl">
+                                Return to jessebubble.dev
+                            </button>
+                        </div>
+                    </Link>
+                    <Pattern />
                 </div>
             </div>
         </div>
